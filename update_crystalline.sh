6b711c3aa82c22f75da1598fc2ffad7c373aee44
Caspian Baska <email@caspian.computer>
2021-11-15 22:48:22 +1100

    scripts: fix install of crystalline

--- update_crystalline.sh
+++ update_crystalline.sh
@@ -7,5 +7,5 @@ crysatlline_path="${HOME}/.local/bin/crystalline"
 curl \
     -L https://github.com/elbywan/crystalline/releases/latest/download/crystalline_x86_64-apple-darwin.gz \
     -o "${crysatlline_path}.gz" \
-    && yes | gzip -d "${crysatlline_path}.gz" \
+    && yes | gzip -f -d "${crysatlline_path}.gz" \
     && chmod u+x "${crysatlline_path}"
